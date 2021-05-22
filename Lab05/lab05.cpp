1088c0a6d94753243bb0a409b083bec1b92d7f92
Govert <car16116@byui.edu>
2021-05-22 02:15:13 -0600

    V0.1 to test

--- Lab05/lab05.cpp
+++ Lab05/lab05.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <string>
+#include <stdio.h> // defines FILENAME_MAX for current directory
+#include <algorithm>
 
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+static const char separator = '/';
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+static const char separator = '\\';
+#endif
 
-int main(int argc, char * argv[]) 
+//Templates
+std::string getTruePath(std::string path);
+std::string normalizePath(std::string path);
+int initialSlashCount(std::string path);
+
+int main()
 {
     std::string path1;
     std::string path2;
@@ -12,29 +28,95 @@ int main(int argc, char * argv[])
 
     std::cout << "Insert second path" << std::endl;
     std::cin >> path2;
-    
-    getTruePath(path1) == getTruePath(path2)
-    //Call get true dire with Path1 and Path2
+
+    //Normalize strings acorting to operating system
+    path1 = normalizePath(path1);
+    path2 = normalizePath(path2);
+
+    std::cout << "The paths are " << ((getTruePath(path1) == getTruePath(path2)) ? "" : "NOT") << " homographs" << std::endl;
 
     return 0;
 }
 
-std::string getTruePath(std::string path) 
+std::string getTruePath(std::string path)
 {
-    std::string sDir = ""; //The current directory
+    char cCurrentPath[FILENAME_MAX];
+    GetCurrentDir(cCurrentPath, FILENAME_MAX);
 
-    for (int i = 0; i < path.length() ;i++)
+    std::string sDir(cCurrentPath); //The default directory
+    //normalize current directory
+    sDir = normalizePath(sDir);
+
+    int slashCount = initialSlashCount(sDir);
+
+    for (int i = 0; i < path.length(); i++)
     {
-        if(path[i] == '.')
+        if (i == 0 && path[0] != separator)
         {
-            //Here is when we find ../
+            path = separator + path;
+            slashCount--;
+        }
+
+        if (path[i] == '.')
+        {
+            //Here is when we find ../ (parent operator)
             if (path[i + 1] == '.')
             {
+                i += 2;
 
+                int tSlash = 0;
+                std::string tDir = "";
+                int dirIterator = 0;
+
+                while (tSlash < slashCount)
+                {
+                    if (sDir[dirIterator] == separator)
+                        tSlash++;
+                    tDir += sDir[dirIterator++];
+                }
+                sDir = tDir;
+
+                //decrement directories
+                slashCount--;
             }
-            else //The path begin with the current directory
-                i++; //This is dummy to avoid errors
+            else if (path[i + 1] == separator) //The path begin with the current directory
+            {
+                i++;
+            }
+            continue; //Next for iteration
         }
+        if (path[i] == separator)
+            slashCount++;
+
+        sDir += path[i];
     }
     return sDir;
+}
+
+std::string normalizePath(std::string path)
+{
+    for (int i = 0; i < path.length(); i++)
+    {
+        if (path[i] == '/' || path[i] == '\\')
+            path[i] = separator;
+    }
+
+    //Transforming to lowercase
+    std::transform(path.begin(), path.end(), path.begin(),
+                   [](unsigned char c)
+                   { return std::tolower(c); });
+
+    return path;
+}
+
+int initialSlashCount(std::string path)
+{
+    int count = 0;
+    for (int i = 0; i < path.length(); i++)
+    {
+        if (path[i] == separator)
+            count++;
+    }
+
+    return count;
 }
