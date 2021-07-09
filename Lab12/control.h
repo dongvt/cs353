fb1d5264b7a2b6efde27d5d85a453da22e829432
Govert <car16116@byui.edu>
2021-07-09 17:52:34 -0600

    initial commmit

---
+++ Lab12/control.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+ * COMPONENT:
+ *    CONTROL
+ * Author:
+ *    Br. Helfrich, Govert Carreno
+ * Summary:
+ *    This class stores the notion of Bell-LaPadula
+ ************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <map>
+
+enum Control {
+    PUBLIC,
+    CONFIDENTIAL,
+    PRIVILEGED,
+    SECRET
+};
+
+bool securityControlRead(Control assetControl, Control subjectControl);
+
+bool securityControlWrite(Control assetControl, Control subjectControl);
